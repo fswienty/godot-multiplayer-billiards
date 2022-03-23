ff3ad0cd9509cb264451a4801b3333acae636797
vrojak <>
2022-03-24 00:11:47 +0100

    better force calculation

--- scripts/pool/queue_controller.gd
+++ scripts/pool/queue_controller.gd
@@ -5,14 +5,11 @@ signal queue_hit
 
 export(float) var distance_at_rest: float = 20.0
 export(float) var max_distance: float = 200.0
-
 export(float) var force_mult = 5
 
-var force: float = 0.0
-var lmb_pressed: bool = false
+var dragged_distance: float = 0.0
 var start_hold_distance: float = 0.0
 var cue_ball: Ball
-var ball_holder: Node2D
 
 onready var queue_container: Node2D = $RotatingContainer
 onready var queue: Sprite = $RotatingContainer/QueueSprite
@@ -26,7 +23,7 @@ func run():
 	if cue_ball == null:
 		printerr("missing cue ball!")
 		return
-	queue.show()
+	queue_container.show()
 	# get needed variables
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	var ball_pos: Vector2 = cue_ball.global_position
@@ -37,15 +34,19 @@ func run():
 	# set queue sprite
 	queue.position.x = -distance_at_rest
 
-	# get force when dragging mouse while clicked
+	# handle dragging while lmb pressed
 	if Input.is_action_just_pressed("lmb"):
 		start_hold_distance = ball_to_mouse.length()
 	if Input.is_action_pressed("lmb"):
-		force = ball_to_mouse.length() - start_hold_distance
-		force = clamp(force, 0, 200)
-		queue.position.x -= force
-	if Input.is_action_just_released("lmb") and force > 0:
-		var impulse: Vector2 = force_mult * force * -ball_to_mouse.normalized()
+		dragged_distance = ball_to_mouse.length() - start_hold_distance
+		dragged_distance = clamp(dragged_distance, 0, max_distance)
+		queue.position.x -= dragged_distance
+	if Input.is_action_just_released("lmb") and dragged_distance > 0:
+		var impulse: Vector2 = (
+			force_mult
+			* (dragged_distance / max_distance)
+			* -ball_to_mouse.normalized()
+		)
 		queue_container.hide()
 		emit_signal("queue_hit", impulse)
 	rpc_unreliable("_set_state", queue.global_position, queue.rotation, queue_container.visible)
